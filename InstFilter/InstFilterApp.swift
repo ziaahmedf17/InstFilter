c40f9b69ac4100eea42c5d8634b6038498d40893
Zia Ahmed <ziaahmedf17@gmail.com>
2025-07-30 04:08:57 +0500

    Initial Commit

---
+++ InstFilter/InstFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstFilterApp.swift
+//  InstFilter
+//
+//  Created by Zi on 30/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct InstFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InstFilterTests/InstFilterTests.swift
@@ -0,0 +1,17 @@
+//
+//  InstFilterTests.swift
+//  InstFilterTests
+//
+//  Created by Zi on 30/07/2025.
+//
+
+import Testing
+@testable import InstFilter
+
+struct InstFilterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
