da3906fbdd5663ed24eb5f8a1a0d7705d848bf55
Zia Ahmed <ziaahmedf17@gmail.com>
2025-07-30 06:51:59 +0500

    Update ContentView.swift

--- InstFilter/ContentView.swift
+++ InstFilter/ContentView.swift
@@ -4,28 +4,41 @@
 //
 //  Created by Zi on 30/07/2025.
 //
+import CoreImage
+import CoreImage.CIFilterBuiltins
 
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showingConfirmation = false
-    @State private var backgroundColor = Color.white
+    @State private var image: Image?
     
     var body: some View {
-        Text("Hello World")
-            .frame(width: 300, height: 300)
-            .background(backgroundColor)
-            .onTapGesture {
-                showingConfirmation = true
-            }
-            .confirmationDialog("Change Background", isPresented: $showingConfirmation) {
-                Button("Red"){backgroundColor = .red}
-                Button("Green"){backgroundColor = .green}
-                Button("Blue"){backgroundColor = .blue}
-                Button("Cancel", role: .cancel){}
-            } message: {
-                Text("Select a new color")
-            }
+        VStack{
+            image?
+                .resizable()
+                .scaledToFit()
+        }
+        .onAppear(perform: loadImage)
+    }
+    
+    func loadImage()
+    {
+        guard let inputImage = UIImage(named: "Example") else { return }
+        let beginImage = CIImage(image: inputImage)
+        
+        let context = CIContext()
+        let currentFilter = CIFilter.twirlDistortion()
+        currentFilter.inputImage = beginImage
+        currentFilter.radius = 1000
+        currentFilter.center = CGPoint(x: inputImage.size.width/2, y: inputImage.size.height/2)
+        
+        guard let outputImage = currentFilter.outputImage else { return }
+        
+        if let cgimg = context.createCGImage(outputImage, from: outputImage.extent)
+        {
+            let uiImage = UIImage(cgImage: cgimg)
+            image = Image(uiImage: uiImage)
+        }
     }
 }
 
